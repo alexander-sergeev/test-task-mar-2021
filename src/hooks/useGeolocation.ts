c9b9fd238f5c52e819c220fe5d8f0d10d1aff238
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-01 13:52:26 +0200

    Implement useGeolocation hook

---
+++ src/hooks/useGeolocation.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+
+export interface GeolocationState {
+  /**
+   * Is user allowed access to Geolocation API
+   */
+  readonly allowed: boolean;
+  /**
+   * 'latitude' property of GeolocationCoordinates
+   */
+  readonly lat?: number;
+  /**
+   * 'longitude' property of GeolocationCoordinates
+   */
+  readonly lng?: number;
+}
+
+const useGeolocation = (): GeolocationState => {
+  const initialState: GeolocationState = {
+    allowed: false,
+  };
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    let canceled = false;
+
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      if (canceled) {
+        return;
+      }
+      setState({
+        allowed: true,
+        lat: coords.latitude,
+        lng: coords.longitude
+      });
+    }, (error) => {
+      if (canceled) {
+        return;
+      }
+      // TODO: Currently it treat any error as user disallowed access, should handle errors properly
+      setState(initialState);
+    }, {
+      enableHighAccuracy: true,
+    });
+
+    return () => {
+      canceled = true;
+    };
+  });
+
+  return state;
+}
+
+export { useGeolocation };
+
