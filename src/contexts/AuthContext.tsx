f3f9656abee4da59ad4be6077c503e4e6bcca735
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-04 12:06:14 +0200

    Store authenticated status in context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import React, { createContext, useCallback, useContext } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 
 interface AuthContextState {
+  authenticated: boolean;
   exchangeCode: (code: string) => Promise<void>;
 }
 
 const INITIAL_STATE: AuthContextState = {
+  authenticated: false,
   exchangeCode: async (code: string) => {},
 };
 
@@ -16,14 +18,17 @@ export type AuthProviderProps = {
 };
 
 export const AuthProvider = (props: AuthProviderProps) => {
+  const [auth, setAuth] = useState(false);
   const exchangeCode = useCallback(async (code: string) => {
     const { data } = await axios.post('/proceedAuth', { code });
     localStorage.setItem('tokens', data.tokens);
+    setAuth(true);
   }, []);
 
   return (
     <AuthContext.Provider
       value={{
+        authenticated: auth,
         exchangeCode,
       }}
     >
