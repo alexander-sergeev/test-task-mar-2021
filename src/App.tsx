d050a8f66cbd9d3e49b0e075189cac3df8502b4d
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-05 12:37:39 +0200

    Add Apollo client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -10,40 +10,48 @@ import LoginCallback from './pages/LoginCallback/LoginCallback';
 import { LanguageConsumer, LanguageProvider } from './contexts/LanguageContext';
 import Profile from './pages/Profile/Profile';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <AuthProvider>
-      <LanguageProvider>
-        <LanguageConsumer>
-          {() => (
-            <Router>
-              <Layout className="layout">
-                <Layout.Header>
-                  <Navbar></Navbar>
-                </Layout.Header>
-                <Layout.Content>
-                  <Switch>
-                    <Route exact path="/">
-                      <Home />
-                    </Route>
-                    <Route exact path="/loginCallback">
-                      <LoginCallback />
-                    </Route>
-                    <ProtectedRoute exact path="/profile">
-                      <Profile />
-                    </ProtectedRoute>
-                    <Route path="/">
-                      <NotFound />
-                    </Route>
-                  </Switch>
-                </Layout.Content>
-              </Layout>
-            </Router>
-          )}
-        </LanguageConsumer>
-      </LanguageProvider>
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <LanguageProvider>
+          <LanguageConsumer>
+            {() => (
+              <Router>
+                <Layout className="layout">
+                  <Layout.Header>
+                    <Navbar></Navbar>
+                  </Layout.Header>
+                  <Layout.Content>
+                    <Switch>
+                      <Route exact path="/">
+                        <Home />
+                      </Route>
+                      <Route exact path="/loginCallback">
+                        <LoginCallback />
+                      </Route>
+                      <ProtectedRoute exact path="/profile">
+                        <Profile />
+                      </ProtectedRoute>
+                      <Route path="/">
+                        <NotFound />
+                      </Route>
+                    </Switch>
+                  </Layout.Content>
+                </Layout>
+              </Router>
+            )}
+          </LanguageConsumer>
+        </LanguageProvider>
+      </AuthProvider>
+    </ApolloProvider>
   );
 };
 
