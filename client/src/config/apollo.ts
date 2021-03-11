51ad901f5fb0d67c4aa379e4dcee9d84ec871bbb
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-11 13:50:47 +0200

    Refresh token and retry query on unauth error

--- client/src/config/apollo.ts
+++ client/src/config/apollo.ts
@@ -1,5 +1,13 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  fromPromise,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { GraphQLError } from 'graphql';
+import { onError } from '@apollo/client/link/error';
+import axios from 'axios';
 
 const httpLink = new HttpLink({ uri: '/graphql' });
 
@@ -17,7 +25,44 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+  if (
+    graphQLErrors?.some(
+      (e: GraphQLError) => e.extensions?.code === 'UNAUTHENTICATED',
+    )
+  ) {
+    return fromPromise(
+      axios
+        .post('/refreshToken', {
+          tokens: JSON.parse(localStorage.getItem('tokens')!),
+        })
+        .then(({ data }) => {
+          localStorage.setItem('tokens', JSON.stringify(data.credentials));
+
+          const oldHeaders = operation.getContext().headers;
+          operation.setContext({
+            headers: {
+              ...oldHeaders,
+              authorization: `Bearer ${data.credentials.access_token}`,
+            },
+          });
+          return true;
+        })
+        .catch((err) => {
+          console.error(err);
+          return false;
+        }),
+    )
+      .filter((value) => Boolean(value))
+      .flatMap(() => {
+        return forward(operation);
+      });
+  }
+});
+
+const link = errorLink.concat(authLink).concat(httpLink);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
