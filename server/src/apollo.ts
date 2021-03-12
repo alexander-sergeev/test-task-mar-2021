15739180cf7054f4b1b96839d0068721c6aea42e
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-12 10:54:52 +0200

    Validate access token audience

--- server/src/apollo.ts
+++ server/src/apollo.ts
@@ -1,8 +1,23 @@
 import { ApolloServer, AuthenticationError } from 'apollo-server-koa';
 import resolvers from './resolvers';
 import typeDefs from './typeDefs';
-import { GOOGLE_API_USERINFO_ENDPOINT } from './constants';
+import {
+  GOOGLE_API_TOKENINFO_ENDPOINT,
+  GOOGLE_API_USERINFO_ENDPOINT,
+} from './constants';
 import getGoogleOauthClient from './utils/getGoogleOauthClient';
+
+interface TokenInfo {
+  azp: string;
+  aud: string;
+  sub: string;
+  scope: string;
+  exp: string;
+  expires_in: string;
+  email: string;
+  email_verified: string;
+  access_type: string;
+}
 
 export const server = new ApolloServer({
   typeDefs,
@@ -15,14 +30,22 @@ export const server = new ApolloServer({
     const oAuth2Client = getGoogleOauthClient();
     oAuth2Client.setCredentials({ access_token: token });
     try {
-      const { data } = await oAuth2Client.request({
+      const {
+        data: tokenInfo,
+      }: { data: TokenInfo } = await oAuth2Client.request({
+        url: GOOGLE_API_TOKENINFO_ENDPOINT,
+      });
+      if (tokenInfo.aud != process.env.GOOGLE_CLIENT_ID) {
+        throw new Error('Token audience mismatch');
+      }
+      const { data: userData } = await oAuth2Client.request({
         url: GOOGLE_API_USERINFO_ENDPOINT,
       });
       return {
-        user: data,
+        user: userData,
       };
     } catch (err) {
-      if (err.response.status === 401) {
+      if (err.response.status === 400 || err.response.status === 401) {
         throw new AuthenticationError('Provided token is invalid');
       }
       throw err;
