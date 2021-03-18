13f6f3b24ddbb8d66dcaa70e9f8d9da465167a32
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-18 20:36:28 +0200

    Cache home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout } from 'antd';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import { AuthProvider } from './contexts/AuthContext';
@@ -12,8 +12,30 @@ import Profile from './pages/Profile/Profile';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './config/apollo';
+import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
 
 const App = () => {
+  /**
+   * About CacheRoute className="cached-route"
+   * react-router-cache-route library creates one more div that wraps our route.
+   * We need to make this div fill the parent, so we add some styles to it
+   */
+  const routerSwitch = (
+    <CacheSwitch>
+      <CacheRoute className="cache-route" exact path="/">
+        <Home />
+      </CacheRoute>
+      <Route exact path="/loginCallback">
+        <LoginCallback />
+      </Route>
+      <ProtectedRoute exact path="/profile">
+        <Profile />
+      </ProtectedRoute>
+      <Route path="/">
+        <NotFound />
+      </Route>
+    </CacheSwitch>
+  );
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
@@ -25,22 +47,7 @@ const App = () => {
                   <Layout.Header>
                     <Navbar></Navbar>
                   </Layout.Header>
-                  <Layout.Content>
-                    <Switch>
-                      <Route exact path="/">
-                        <Home />
-                      </Route>
-                      <Route exact path="/loginCallback">
-                        <LoginCallback />
-                      </Route>
-                      <ProtectedRoute exact path="/profile">
-                        <Profile />
-                      </ProtectedRoute>
-                      <Route path="/">
-                        <NotFound />
-                      </Route>
-                    </Switch>
-                  </Layout.Content>
+                  <Layout.Content>{routerSwitch}</Layout.Content>
                 </Layout>
               </Router>
             )}
