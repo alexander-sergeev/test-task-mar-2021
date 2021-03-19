3e720a4efc16852d240255554692fe9e242c9a2e
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-19 14:52:16 +0200

    Implement requireAuth middleware

---
+++ server/src/middlewares/requireAuth.ts
@@ -0,0 +1,42 @@
+import Koa from 'koa';
+import getGoogleOauthClient from '../utils/getGoogleOauthClient';
+import logger from '../logger';
+import { ID_TOKEN_HTTP_HEADER_NAME } from '../constants';
+
+/**
+ * Koa middleware, checks for authorization and injects user info into context state.
+ * If user is not authenticated, end request with 401 error
+ */
+export const requireAuth: Koa.Middleware = async (
+  ctx: Koa.Context,
+  next: Koa.Next,
+) => {
+  const authorizationHeader = ctx.req.headers.authorization;
+  const idToken = ctx.req.headers[ID_TOKEN_HTTP_HEADER_NAME];
+  if (!authorizationHeader) {
+    ctx.status = 401;
+    throw new Error('Authorization header is not provided');
+  }
+  if (typeof idToken !== 'string') {
+    ctx.status = 401;
+    throw new Error('ID Token is not provided');
+  }
+  const oAuth2Client = getGoogleOauthClient();
+  try {
+    const loginTicket = await oAuth2Client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const tokenPayload = loginTicket.getPayload();
+    ctx.state.user = {
+      name: tokenPayload?.name,
+      picture: tokenPayload?.picture,
+      locale: tokenPayload?.locale,
+    };
+  } catch (err) {
+    logger.debug(err);
+    ctx.status = 401;
+    throw new Error('Provided ID Token is invalid');
+  }
+  return next();
+};
