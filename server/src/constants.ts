5c06dc410aa5875314be05ff9e816be0563af0cf
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-03-19 17:27:55 +0200

    Other log level and JSON logs in prod

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,8 +2,11 @@ export const GOOGLE_API_USERINFO_ENDPOINT =
   'https://www.googleapis.com/oauth2/v3/userinfo';
 export const GOOGLE_API_TOKENINFO_ENDPOINT =
   'https://www.googleapis.com/oauth2/v3/tokeninfo';
+
 export const ID_TOKEN_HTTP_HEADER_NAME = 'x-authentication';
 
-export const LOG_LEVEL = 'debug';
+export const PRODUCTION = process.env.NODE_ENV === 'production';
+
+export const LOG_LEVEL = PRODUCTION ? 'error' : 'debug';
 
 export const GRAPHQL_PATH = '/graphql';
--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,10 +1,10 @@
 import pino from 'pino';
 import Koa from 'koa';
-import { LOG_LEVEL } from '../constants';
+import { LOG_LEVEL, PRODUCTION } from '../constants';
 
 const logger: pino.Logger = pino({
   level: LOG_LEVEL,
-  prettyPrint: true,
+  prettyPrint: !PRODUCTION,
 });
 
 export const koaFormatFromCtx = (ctx: Koa.Context): string => {
